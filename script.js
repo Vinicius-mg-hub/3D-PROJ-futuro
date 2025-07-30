d3e3416c7e09164c84a53c343bcec7422b374204
Vinicius-mg-hub <vinicius.martins.guimaraes@escola.pr.gov.br>
2025-07-30 12:14:33 -0300

    n

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const perguntas = [
     {
-        enunciado: "",
+        enunciado: "Qual vocẽ acredita que deve ser o pruncipal objetio de um jovem no inicio de sua carreira?
         alternativas: [
             {
-                texto:"",
-                afirmação: ""
+                texto:"a",
+                afirmação: "Priorizar experiẽncias, mesmo que o salário seja baixo"
             },
             {
-                texto: "",
-                afirmação: ""
+                texto: "b",
+                afirmação: "Priorizar o salário, mesmo que as vagas ofereçam pouca experiência"
             }
         ]
     },
